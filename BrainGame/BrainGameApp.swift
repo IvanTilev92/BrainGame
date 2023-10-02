44a071788b188bd5ac7be7825a8d09b6d24acbb8
IvanTilev <iivantilev@gmail.com>
2023-10-02 22:27:19 +0300

    Initial Commit

---
+++ BrainGame/BrainGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainGameApp.swift
+//  BrainGame
+//
+//  Created by Ivan Tilev on 2.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BrainGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
