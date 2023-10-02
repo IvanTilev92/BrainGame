229ea455d81858fa5cf5a08d3c32bf0cd8332abb
IvanTilev <iivantilev@gmail.com>
2023-10-02 22:51:27 +0300

    Creting the fist view that contains the player score, a random computer move and that objective to win to lose

--- BrainGame/ContentView.swift
+++ BrainGame/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var possibleMoves = ["Rock", "Paper", "Scissors"]
+    
+    @State private var score = 0
+    
+    @State private var currentState = ""
+    @State private var shouldWin = Bool.random()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.init(red: 0.1, green: 0.3, blue: 0.2)
+            VStack(spacing: 20) {
+                Text("Player score is: \(score)")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                    .foregroundColor(.white)
+                Text("Computer move is: \(possibleMoves[Int.random(in: 0..<possibleMoves.count)])")
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.white)
+                if shouldWin {
+                    Text("You have to WIN")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .bold()
+                } else {
+                    Text("You have to LOSE")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .bold()
+                }
+            }
+            .padding(20)
+            .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
     }
 }
 
